9ff974ea65c916b9429073a865872b3cf2f1cfd1
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-02-10 14:54:12 +0100

    fix(qav3): fix path handling to journal storage
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- quickadapter/user_data/freqaimodels/XGBoostRegressorQuickAdapterV35.py
+++ quickadapter/user_data/freqaimodels/XGBoostRegressorQuickAdapterV35.py
@@ -69,7 +69,6 @@ class XGBoostRegressorQuickAdapterV35(BaseRegressionModel):
         start = time.time()
         if self.__optuna_hyperopt:
             study_name = str(dk.pair)
-            storage_dir = str(dk.full_path).rsplit("/", 1)
             pruner = optuna.pruners.HyperbandPruner()
             study = optuna.create_study(
                 study_name=study_name,
@@ -80,7 +79,7 @@ class XGBoostRegressorQuickAdapterV35(BaseRegressionModel):
                 pruner=pruner,
                 direction=optuna.study.StudyDirection.MINIMIZE,
                 storage=optuna.storages.journal.JournalFileBackend(
-                    f"{storage_dir}/optuna-xgboost-{sanitize_path(study_name)}.log"
+                    f"./optuna-xgboost-{sanitize_path(study_name)}.log"
                 ),
                 load_if_exists=True,
             )
