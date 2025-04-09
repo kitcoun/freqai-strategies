69b76abaeecf9b7967ab7e15f93524e470d7cd2b
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-04-09 20:35:44 +0200

    perf(qav3): fine tune natr ratio optuna dimension
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- quickadapter/user_data/freqaimodels/QuickAdapterRegressorV3.py
+++ quickadapter/user_data/freqaimodels/QuickAdapterRegressorV3.py
@@ -44,7 +44,7 @@ class QuickAdapterRegressorV3(BaseRegressionModel):
     https://github.com/sponsors/robcaulk
     """
 
-    version = "3.7.3"
+    version = "3.7.4"
 
     @cached_property
     def _optuna_config(self) -> dict:
@@ -944,7 +944,7 @@ def label_objective(
         max_label_period_candles,
         step=candles_step,
     )
-    label_natr_ratio = trial.suggest_float("label_natr_ratio", 0.0625, 0.175)
+    label_natr_ratio = trial.suggest_float("label_natr_ratio", 0.0675, 0.175)
 
     _, peak_values, _ = dynamic_zigzag(
         df,
