57f4945de879a3d34f4cd1083c19610d0dbdbe76
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-05-10 15:33:50 +0200

    perf(qav3): tune TP NATR ratio
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- quickadapter/user_data/strategies/QuickAdapterV3.py
+++ quickadapter/user_data/strategies/QuickAdapterV3.py
@@ -372,7 +372,7 @@ class QuickAdapterV3(IStrategy):
         return self.get_label_natr_ratio(pair) * 0.75
 
     def get_take_profit_natr_ratio(self, pair: str) -> float:
-        return self.get_label_natr_ratio(pair) * 0.5
+        return self.get_label_natr_ratio(pair) * 0.65
 
     def set_freqai_targets(self, dataframe: DataFrame, metadata: dict, **kwargs):
         pair = str(metadata.get("pair"))
